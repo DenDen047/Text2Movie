1eb0e77d1626056cfcf4b51540ad300185122266
DenDen047 <sh.mn.nat@gmail.com>
2019-04-03 21:04:38 +0900

    それぞれの処理をからの関数に置き換えた

--- src/main.py
+++ src/main.py
@@ -3,16 +3,30 @@ import sys
 import re
 import glob
 
+import cv2
+
+
+def read_markdown():
+    return None
+
+
+def analysis_markdown(md_file):
+    return None
+
+
+def generate_movie(source):
+    pass
+
 
 def main():
     # MarkDownファイルを読み込む
-    pass
+    md_file = read_markdown()
 
     # MDの構文を解析
-    pass
+    source = analysis_markdown(md_file)
 
     # 動画を生成
-    pass
+    generate_movie(source)
 
 
 if __name__ == '__main__':
