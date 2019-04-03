215d967712ba64f60b6538c245a094767a1f5b7d
DenDen047 <sh.mn.nat@gmail.com>
2019-04-03 21:13:42 +0900

    動画生成部分の構造を少し明確化

--- src/main.py
+++ src/main.py
@@ -14,8 +14,25 @@ def analysis_markdown(md_file):
     return None
 
 
-def generate_movie(source):
-    pass
+def text_frame(text):
+    frames = [None]
+    return frames
+
+def generate_movie(source, width, height, result_path):
+    # 形式はMP4Vを指定
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    fps = 20
+    # 出力先のファイルを開く
+    out = cv2.VideoWriter(result_path, int(fourcc), fps, (int(width), int(height)))
+
+    # 動画を生成
+    for part in source:
+        # 要素の性質に分けて処理
+        if part['type'] == 'text':
+            frames = text_frame(part['text'])
+        # 生成したフレームを書き込み
+        for f in frames:
+            out.write(f)
 
 
 def main():
@@ -26,7 +43,10 @@ def main():
     source = analysis_markdown(md_file)
 
     # 動画を生成
-    generate_movie(source)
+    result_path = 'output.m4v'
+    width = 720
+    height = 480
+    generate_movie(source, width, height, result_path)
 
 
 if __name__ == '__main__':
